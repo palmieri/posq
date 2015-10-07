a344b481cf211078ddf9432eb533349925f7e3c7
Billy Okal <okal.billy@googlemail.com>
2015-10-07 12:36:15 +0200

    Example runs in Python

--- Python/demo.py
+++ Python/demo.py
@@ -3,7 +3,7 @@ import numpy as np
 import posq
 
 source = np.array([0, 0, 0])
-target = np.array([-1, 0, np.pi/2])
+target = np.array([1, 1, -np.pi/6])
 direction = 1
 resolution = 0.1
 base = 0.4
@@ -17,7 +17,21 @@ traj, speedvec, vel, inct = posq.integrate(source, target, direction,
 
 
 import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
 
 plt.figure(figsize=(10, 10))
-plt.plot(traj[:, 0], traj[:, 0])
+plt.plot(traj[0, 0], traj[0, 0], ls='', marker='8', color='b',
+         markersize=15, label='start')
+plt.plot(traj[-1, 0], traj[-1, 0], ls='', marker='8', color='g',
+         markersize=15, label='goal')
+
+ax = plt.axes()
+for wp in traj:
+    vx, vy = np.cos(wp[2]), np.sin(wp[2])
+    ax.arrow(wp[0], wp[1], 0.1*vx, 0.1*vy, head_width=0.02,
+             head_length=0.02, lw=0.4, fc='brown', ec='brown')
+
+plt.xlim((-0.2, 1.2))
+plt.ylim((-0.2, 1.2))
+# plt.savefig('posq_demo.pdf')
 plt.show()
